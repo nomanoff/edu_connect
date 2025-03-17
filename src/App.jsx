f67edecb28b684b13446692b0b00cac677df8fb0
AzeezSWE <im.nomanoff@gmail.com>
2025-03-17 22:07:19 +0900

    fix: css removed margin/padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import MainRoutes from "./routes/MainRoutes.jsx";
+import "./App.css";
 
 function App() {
   const [authState, setAuthState] = useState({
-    isAuthenticated: false,
-    role: "",
+    isAuthenticated: true,
+    role: "admin",
   });
 
   return (
