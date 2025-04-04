f0b2d6cab1616c27abf05f32d1c363e9b19be0e2
isakjanofv <isakjanovorg@gmail.com>
2025-04-04 18:47:27 +0500

    fix: conflict

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -13,7 +13,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: center; 
   align-items: center;
   height: 100vh;
 `;
