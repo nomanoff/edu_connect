9483734a05cde5f20ead045b087f237865389ce2
Anvarbek <c1221sharp@gmail.com>
2025-04-22 23:55:44 +0500

    feat: add detail

--- src/components/admin/AddNewTeacher.jsx
+++ src/components/admin/AddNewTeacher.jsx
@@ -1,7 +1,11 @@
 import { useDispatch } from "react-redux";
-import styled from "styled-components";
+import { useState } from "react";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { Dialog } from "@mui/material";
 
 import { registerTeacherTokenAsync } from "../../utils/redux/teacherSlice";
+
+import styled from "styled-components";
 import { EdButton_admin, EdH1 } from "../EdStyled";
 
 const Wrapper = styled.div`
@@ -9,14 +13,50 @@ const Wrapper = styled.div`
   border-radius: 5px;
   margin: 20px;
   border: 2px solid #808080;
-  padding: 20px;`
-;
+  padding: 20px;
+`;
+
+const Button = styled.button`
+  font-size: 1.3rem;
+  width: 150px;
+  height: 50px;
+  color: #fff;
+  text-align: center;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: none;
+  cursor: pointer;
+  background-color: #0082f5;
+  border-radius: 10px;
+  float: right;
+`;
+
+const Dialog1 = styled.div`
+  width: 350px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
 
 const AddNewTeacher = () => {
+  const [open, setOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboard = () => {
+    // navigator.clipboard.;
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
+
   const dispatch = useDispatch();
+  
 
   const handleCreateTeacher = () => {
     dispatch(registerTeacherTokenAsync());
+    setOpen(true);
   };
 
   return (
@@ -34,6 +74,21 @@ const AddNewTeacher = () => {
       >
         Generate Key
       </EdButton_admin>
+
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <EdH1 fontSize="1.3rem" fontWeight={"700"}>
+        Generate Copy Token
+        </EdH1>
+        <Dialog1>
+          
+          <EdH1 margin={"0 40px 0 0"}>
+            
+          </EdH1>
+          <Button onClick={copyToClipboard}>
+            copy {copied ? "✅" : <ContentCopyIcon />}
+          </Button>
+        </Dialog1>
+      </Dialog>
     </Wrapper>
   );
 };
