d475be50506846d4641d0ec5f2b6d2eab93630ee
AsadbekSWE <asadbekgulomov2010@gmail.com>
2025-05-17 18:59:25 +0500

    manage students finished

--- src/pages/parent/ParentSettings.jsx
+++ src/pages/parent/ParentSettings.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { useState } from "react";
 import styled from "styled-components";
 
